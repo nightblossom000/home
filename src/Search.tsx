5c922e426603f3a3185a61178cfb451723cdb660
nightblossom000 <alvand.malikh@gmail.com>
2024-07-29 00:50:26 +0000

    test

--- src/Search.tsx
+++ src/Search.tsx
@@ -2,10 +2,25 @@ import { Navbar } from "./components/Navbar";
 import PostList from './components/PostList';
 import { Toolbar } from "./components/Toolbar";
 
+import { useLocation } from 'react-router-dom';
+    
+    
+            
+                
+    
+                
+    
+            
 function Search() {
+const location = useLocation();
+                const searchParams = new URLSearchParams(location.search);
+                const query = searchParams.get('q');
   return (
     <>
       <Navbar />
+      <div>
+                        <h1>Search Results for: {query}</h1>
+                    </div>
       <PostList />
       <Toolbar />
       
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { HashLink as Link } from 'react-router-hash-link';
+import { useNavigate } from "react-router-dom";
+
 
 import {
   NavigationMenu,
@@ -44,7 +46,16 @@ const routeList: RouteProps[] = [
   },
 ];
 
+
+
 export const Navbar = () => {
+const navigate = useNavigate();
+
+const search = (e) => {
+  e.preventDefault();
+  return navigate("/search?q=" + e.target.q.value);
+} 
+
   const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
     <header className="sticky border-b-[1px] top-0 z-40 w-full bg-white dark:border-b-slate-700 dark:bg-background">
@@ -53,12 +64,13 @@ export const Navbar = () => {
           <NavigationMenuItem className="font-bold flex">
             
             
-<form method="GET" action="/search">
+<form method="POST"  onSubmit={search}>
     <div className="relative text-gray-600 focus-within:text-gray-400">
       <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-        <button type="submit" className="p-1 focus:outline-none focus:shadow-outline">
+      <button type="submit" className="p-1 focus:outline-none focus:shadow-outline">
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" className="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </button>
+       
       </span>
       <input type="search" name="q" className="py-2 text-sm border-2 dark:border-b-slate-700 dark:bg-white bg-background rounded-md pl-10 focus:outline-none focus:bg-white focus:text-gray-900" placeholder="Search..." autoComplete="off" />
     </div>
